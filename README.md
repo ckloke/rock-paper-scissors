c60d159ca48a9edc8747a01d4a72c6f8e4f709e2
ckloke <40362894+ckloke@users.noreply.github.com>
2021-03-21 01:13:54 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Rock Paper Scissors
+A single-player implementation of the critically acclaimed game <i>Rock Paper Scissors</i> against a computer opponent.
+
+## Features
+There are three difficulty levels:
+* Easy
+* Medium
+* Hard*
+
+*Note - Hard difficulty is extremely hard! Make sure to practice on lower difficulties before attempting!
---
+++ rock_paper_scissors.py
@@ -0,0 +1,81 @@
+import random
+
+# Start
+print("Rock Paper Scissors!")
+print("--------------------------------")
+
+# Get difficulty
+while True:
+    response = input("Enter difficulty (easy, medium, hard): ")
+    if response == "easy":
+        difficulty = "easy"
+        break
+    elif response == "medium":
+        difficulty = "medium"
+        break
+    elif response == "hard":
+        difficulty = "hard"
+        break
+    else:
+        print("that's not a difficulty dummy!")
+
+# Choose move
+while True:
+    move = input("Choose rock, paper, or scissors: ")
+    if move == "rock" or move == "paper" or move == "scissors":
+        break
+    else:
+        print("that's not an option dummy!")
+
+# AI choosing move
+if difficulty == "easy":
+    if move == "rock":
+        ai_move = "scissors"
+    elif move == "paper":
+        ai_move = "rock"
+    elif move == "scissors":
+        ai_move = "paper"
+elif difficulty == "medium":
+    rng = random.randint(1, 3)
+    if rng == 1:
+        ai_move = "rock"
+    elif rng == 2:
+        ai_move = "paper"
+    elif rng == 3:
+        ai_move = "scissors"
+elif difficulty == "hard":
+    if move == "rock":
+        ai_move = "paper"
+    elif move == "paper":
+        ai_move = "scissors"
+    elif move == "scissors":
+        ai_move = "rock"
+
+# Some messages
+print()
+print("You picked:\t", move)
+print("AI picked:\t", ai_move)
+print()
+
+# Determine winner
+if move == ai_move:
+    print("Tie!")
+elif move == "rock":
+    if ai_move == "paper":
+        print("You lose!")
+    elif ai_move == "scissors":
+        print("You win!")
+elif move == "paper":
+    if ai_move == "rock":
+        print("You win!")
+    elif ai_move == "scissors":
+        print("You lose!")
+elif move == "scissors":
+    if ai_move == "rock":
+        print("You lose!")
+    elif ai_move == "paper":
+        print("You win!")
+
+# End
+print("--------------------------------")
+input("Enter anything to quit...")
